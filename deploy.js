6fedcd72ad0705086e2cefdf2e30922f891461b3
Uddeshya Barnwal <uddeshya.barnwal@valuence.inc>
2022-02-04 01:26:55 +0530

    Message and update mesaage

---
+++ deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode} = require('./compile');
+
+const provider = new HDWalletProvider(
+    'huge aerobic bounce belt wink bronze nominee level clean worth outdoor approve',
+    'https://rinkeby.infura.io/v3/d0227bb35bac4fb9a1eeb5aa2eadecda'
+
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log("Attempting to deploy from the acount", accounts[0]);
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+     .deploy({data:bytecode, arguments:['Hi There!']})
+    .send({gas:'1000000', from:accounts[0]});
+
+console.log('Contract deployed to', result.options.address);
+provider.engine.stop();
+
+};
+deploy();
