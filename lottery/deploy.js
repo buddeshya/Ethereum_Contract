af8716be7baa8bcc41e7587f2444433b1a469782
Uddeshya Barnwal <uddeshya.barnwal@valuence.inc>
2022-02-05 02:05:37 +0530

    Lottery sstem

--- lottery/deploy.js
+++ lottery/deploy.js
@@ -1,13 +1,11 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
 
-const { abi, evm } = require('./compile');
-
-provider = new HDWalletProvider(
+const provider = new HDWalletProvider(
   'huge aerobic bounce belt wink bronze nominee level clean worth outdoor approve',
     'https://rinkeby.infura.io/v3/d0227bb35bac4fb9a1eeb5aa2eadecda'
 );
-
 const web3 = new Web3(provider);
 
 const deploy = async () => {
@@ -15,8 +13,8 @@ const deploy = async () => {
 
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(abi)
-    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
